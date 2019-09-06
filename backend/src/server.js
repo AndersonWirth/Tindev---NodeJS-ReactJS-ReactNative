aabb8eaae55655cce0c02148aab3cb58cb37c7e7
Anderson <andersonwirth@gmail.com>
2019-09-05 22:50:14 -0300

    Doc: Alterações realizadas desde o inicio da aplicação na aplicação

---
+++ backend/src/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const routes = require('./routes');
+
+const server = express();
+
+mongoose.connect('mongodb+srv://andersondeveloper:andersondeveloper@cluster0-w1xni.mongodb.net/bancoAnderson?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+});
+
+server.use(cors());
+server.use(express.json());
+
+server.use(routes);
+
+server.listen(3333);
+
