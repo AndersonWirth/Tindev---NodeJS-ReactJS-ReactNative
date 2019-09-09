3cb5ddbb40d3330561dadeaf07d05dd4e0ac7976
AndersonWirth <andersonwirth1@gmail.com>
2019-09-09 20:38:35 -0300

    Doc: Correção feita para não duplicar as imagens dos usuarios

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -7,11 +7,11 @@ import like from '../assets/like.svg';
 import api from '../services/api';
 
 
-export default function Main( match ) {
+export default function Main(match) {
     const [users, setUsers] = useState([]);
 
     useEffect(() => {
-        async function loadUsers(){
+        async function loadUsers() {
             const response = await api.get('/devs', {
                 headers: {
                     user: match.match.params.id,
@@ -23,15 +23,14 @@ export default function Main( match ) {
 
         loadUsers();
     }, [match.match.params.id])
-    
+
     return (
         <div className="main-container">
             <img src={logo} alt="Tindev" />
             <ul>
                 {users.map(user => (
                     <li key={user._id}>
-                        <img src={user.avatar} alt={user.name}/>
-                        <img src={user.avatar} alt={user.name}/>
+                        <img src={user.avatar} alt={user.name} />
                         <footer>
                             <strong>{user.name}</strong>
                             <p>{user.bio}</p>
@@ -39,10 +38,10 @@ export default function Main( match ) {
 
                         <div className="buttons">
                             <button type="button">
-                                <img src={dislike} alt="Dislike"/>
+                                <img src={dislike} alt="Dislike" />
                             </button>
                             <button type="button">
-                                <img src={like} alt="Like"/>
+                                <img src={like} alt="Like" />
                             </button>
                         </div>
                     </li>
